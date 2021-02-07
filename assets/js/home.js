8072127738d15dd5e5ac452c158ac26deee92737
gustavo palandi <gustavoppo@hotmail.com>
2021-02-07 00:53:12 -0300

    Finished project

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,17 +2,17 @@
 // função para a fase fácil
 
 function openF(){
-	window.open("facil.html","_self");
+	window.open("./assets/pag/facil.html","_self");
 }
 
 // função para a fase normal
 
 function openN(){
-	window.open("normal.html","_self");
+	window.open("./assets/pag/normal.html","_self");
 }
 
 // função para a fase difícil
 
 function openD(){
-	window.open("dificil.html","_self");
+	window.open("./assets/pag/dificil.html","_self");
 }
